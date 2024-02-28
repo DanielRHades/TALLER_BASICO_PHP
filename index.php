38bf57d7892b006251ba83e2ab7c997bf0a3a0ee
DanielRHades <daniel7002017@hotmail.coms>
2024-02-27 19:07:27 -0500

    Creacion_Index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="Seccion_Daniel.php" method="get">
+
+<button type="submit">Seccion#1 - Cifrando Digitos</button>
+
+</form>
+
+<form action="Seccion_Esteban.php" method="get">
+
+<button type="submit">Seccion#2 - Fiesta Bucaramanga</button>
+
+</form>
+    
+</body>
+</html>
+
